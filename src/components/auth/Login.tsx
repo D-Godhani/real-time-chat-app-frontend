62f9560ef8b6dbd099dc1a0502de2e84a4e4088d
Dharmik Godhani <122303920+D-Godhani@users.noreply.github.com>
2025-12-02 20:50:27 +0530

    Update Login.tsx

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -14,7 +14,7 @@ const Login = () => {
         error={error}
       >
         <Link to={"/signup"} style={{ alignSelf: "center" }}>
-          <MUILink>Signup mt kar!!</MUILink>
+          <MUILink>Signup karle mamu!!!</MUILink>
         </Link>
       </Auth>
     </>
